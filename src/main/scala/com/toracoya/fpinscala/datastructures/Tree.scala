ccb5569c7d519591532a3f994d40edfe4f029517
Takanori Ikeda <takanori.ikeda@toracoya.com>
2018-09-25 16:42:18 +0900

    Add Tree

---
+++ src/main/scala/com/toracoya/fpinscala/datastructures/Tree.scala
@@ -0,0 +1,58 @@
+package com.toracoya.fpinscala.datastructures
+
+sealed trait Tree[+A]
+
+case class Leaf[A](value: A) extends Tree[A]
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def size[A](tree: Tree[A]): Int = {
+    tree match {
+      case Leaf(_) => 1
+      case Branch(left, right) =>
+        1 + size(left) + size(right)
+    }
+  }
+
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(value) => value
+    case Branch(l, r) =>
+      maximum(l) max maximum(r)
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree {
+    case Leaf(_) => 1
+    case Branch(l, r) =>
+      1 + (depth(l) max depth(r))
+  }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(value) => Leaf(f(value))
+    case Branch(l, r) =>
+      Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B = tree match {
+    case Leaf(value) => f(value)
+    case Branch(l, r) =>
+      g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](tree: Tree[A]): Int = {
+    fold(tree)(_ => 1)(_ + _)
+  }
+
+  def maximumViaFold(tree: Tree[Int]): Int = {
+    fold(tree)(e => e)(_ max _)
+  }
+
+  def depthViaFold[A](tree: Tree[A]): Int = {
+    fold(tree)(_ => 1)((d1, d2) => (d1 max d2) + 1)
+  }
+
+  def mapViaFold[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
+    fold(tree)(a => Leaf(f(a)): Tree[B])(Branch.apply)
+  }
+}
